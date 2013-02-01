49e1c2e6ea95c9951cf2cda9d5ea60818d03a6b3
Joram <jmm308@jaguar1.usouthal.edu>
2013-02-01 16:54:43 -0600

    Auto generated method comments to remove VS2012 warnings

--- Rhino.Security.Windsor/RhinoSecurityInstaller.cs
+++ Rhino.Security.Windsor/RhinoSecurityInstaller.cs
@@ -13,7 +13,11 @@ namespace Rhino.Security.Windsor
 	/// </summary>
 	public class RhinoSecurityInstaller : IWindsorInstaller
 	{
-		public void Install(IWindsorContainer container, IConfigurationStore store)
+	    /// <summary>
+	    /// Performs the installation in the <see cref="T:Castle.Windsor.IWindsorContainer"/>.
+	    /// </summary>
+	    /// <param name="container">The container.</param><param name="store">The configuration store.</param>
+	    public void Install(IWindsorContainer container, IConfigurationStore store)
 		{
 			container.Register(
 				Component.For<IAuthorizationService>()
